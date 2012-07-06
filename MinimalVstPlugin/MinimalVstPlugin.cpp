ccad805e4285262138c2afb8edf30ce4bdab16d3
hotwatermorning <hotwatermorning@gmail.com>
2012-07-07 02:42:04 +0900

    add comments. remove unnecessary non-synth setting. convert tabs to spaces

--- MinimalVstPlugin/MinimalVstPlugin.cpp
+++ MinimalVstPlugin/MinimalVstPlugin.cpp
@@ -12,46 +12,51 @@
 namespace hwm {
 
 struct MinimalVstPlugin
-	:	AudioEffectX
+    //! AudioEffextXを継承する
+    :   AudioEffectX
 {
-	MinimalVstPlugin	(audioMasterCallback audioMaster)
-		:	AudioEffectX(
-				audioMaster,
-				0,	//プログラム数
-				0	//パラメータ数
-				)
-	{
-		setNumInputs(2);
-		setNumOutputs(2);
+    MinimalVstPlugin    (audioMasterCallback audioMaster)
+        :   AudioEffectX(
+                audioMaster,
+                0,  //プログラム数
+                0   //パラメータ数
+                )
+    {
+        //! 出入力チャンネルの設定
+        setNumInputs(2);
+        setNumOutputs(2);
 
-		canProcessReplacing();
-		isSynth(false);
-		setUniqueID('hwm1');
-	}
-	
-	//! 信号処理
-	virtual	void		processReplacing(
-							float		**	inputs,
-							float		**	outputs,
-							VstInt32		sampleFrames )
-	{
-		enum { left, right };
-	
-		//! 左右のチャンネルを入れ替えるだけ
-		for(size_t fr = 0; fr < sampleFrames; ++fr) {
-			float const tmp_l = inputs[right][fr];
-			float const tmp_r = inputs[left][fr];
-		
-			outputs[left][fr] = tmp_l;
-			outputs[right][fr] = tmp_r;
-		}
-	}
+        //! processReplacing関数を使用する
+        canProcessReplacing();
+
+        //! uniqueIDの設定
+        setUniqueID('hwm1');
+    }
+
+    //! 信号処理
+    virtual void        processReplacing(
+                            float       **  inputs,
+                            float       **  outputs,
+                            VstInt32        sampleFrames )
+    {
+        enum { left, right };
+
+        //! 左右のチャンネルを入れ替えるだけ
+        for(size_t fr = 0; fr < sampleFrames; ++fr) {
+            float const tmp_l = inputs[right][fr];
+            float const tmp_r = inputs[left][fr];
+
+            outputs[left][fr] = tmp_l;
+            outputs[right][fr] = tmp_r;
+        }
+    }
 };
 
-}	//namespace hwm
+}   //namespace hwm
 
+//! HOSTがVSTをロード使用としたときに呼ばれるファクトリ関数
 AudioEffect *
-		createEffectInstance(audioMasterCallback audioMaster)
+        createEffectInstance(audioMasterCallback audioMaster)
 {
-	return new hwm::MinimalVstPlugin(audioMaster);
+    return new hwm::MinimalVstPlugin(audioMaster);
 }
